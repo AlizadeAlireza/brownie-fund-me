d79c05d9eafdfe9e65934665bb636ef94b45904b
alireza <95475261+AlizadeAlireza@users.noreply.github.com>
2022-10-05 17:49:22 +0330

    Update test_fund_me.py

--- tests/test_fund_me.py
+++ tests/test_fund_me.py
@@ -1,32 +1,43 @@
 from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from scripts.deploy import deploy_fund_me
-from brownie import network, accounts, exceptions
+from brownie import accounts, FundMe, network, exceptions
 import pytest
 
 
-def test_can_find_and_withdraw():
+def test_can_fund_and_withdraw():
+    """in this section we declare account, deploy contract and set entrance fee
+    after we assert the address of that mapping equal to the value after and before withdraw"""
+    # arrange
     account = get_account()
+
+    # act
     fund_me = deploy_fund_me()
-    # garb the entrance fee
     entrance_fee = fund_me.getEntranceFee() + 100
-    # let's fund it
     tx = fund_me.fund({"from": account, "value": entrance_fee})
     tx.wait(1)
+
+    # assert
     assert fund_me.addressToAmountFunded(account.address) == entrance_fee
 
-    tx2 = fund_me.withdraw({"from": account})
-    tx2.wait(1)
+    tx_2 = fund_me.withdraw({"from": account})
+    tx_2.wait(1)
+
     assert fund_me.addressToAmountFunded(account.address) == 0
 
 
-# the owner can withdraw and nobody else can do
-
-
 def test_only_owner_can_withdraw():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+    """in this section we want to know a different account can withdraw
+    or not?!"""
+    if (
+        network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+    ):  # means that on live network
         pytest.skip("only for local testing")
+
+    # arrange
+    bad_actor = accounts.add()  # generate a random account
     fund_me = deploy_fund_me()
-    bad_actor = accounts.add()
+
     fund_me.withdraw({"from": bad_actor})
     with pytest.raises(exceptions.VirtualMachineError):
+        # if this reverts with virtual machine error, that's good
         fund_me.withdraw({"from": bad_actor})
